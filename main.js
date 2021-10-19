de5e4fd8e5f41545025d31405888c639bf4df9a8
HotAlphaPinkOrAnanya <89530278+HotAlphaPinkOrAnanya@users.noreply.github.com>
2021-10-19 16:47:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+body {
+margin: 0;
+padding-top: 800px;
+text-align:center;
+}
+
+#welcome {
+  position:absolute;
+  overflow:hidden;
+  height:800px;
+  color:#000000;
+  left:0;
+  top:0;
+  right:0;
+  bottom:0;
+  opacity: 0.83;
+    background: linear-gradient(to right, blue 0%,  dodgerblue 100%);
+ display: table;
+    width: 100%;
+    height: 100%;
+}
+h1 {
+  font-family: 'Sigmar One', cursive;
+  font-size: 40px;
+    line-height: 1.2;
+  font-weight: 300;
+}
+h2{
+  font-family: 'Squada One', cursive;
+}
+.line {
+    height: 6px;
+    background-color: #000;
+    width: 70px;
+  margin: 8px auto;}
+img {
+  width:300px;
+  margin:50px;
+  border-radius:90px;
+}
+  
+p{
+  font-weight:bold;
+}
